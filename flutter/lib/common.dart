95e01ed061aef6015a2553d00f76ee1c68ae63f9
zylm00 <1123317122@qq.com>
2025-11-29 14:16:54 +0800

    Update common.dart

--- flutter/lib/common.dart
+++ flutter/lib/common.dart
@@ -3585,24 +3585,16 @@ Widget loadPowered(BuildContext context) {
   if (bind.mainGetBuildinOption(key: "hide-powered-by-me") == 'Y') {
     return SizedBox.shrink();
   }
-  return MouseRegion(
-    cursor: SystemMouseCursors.click,
-    child: GestureDetector(
-      onTap: () {
-        launchUrl(Uri.parse('https://rustdesk.com'));
-      },
-      child: Opacity(
-          opacity: 0.5,
-          child: Text(
-            translate("powered_by_me"),
-            overflow: TextOverflow.clip,
-            style: Theme.of(context)
-                .textTheme
-                .bodySmall
-                ?.copyWith(fontSize: 9, decoration: TextDecoration.underline),
-          )),
-    ),
-  ).marginOnly(top: 6);
+  return Opacity(
+      opacity: 0.5,
+      child: Text(
+        translate("powered_by_me"),
+        overflow: TextOverflow.clip,
+        style: Theme.of(context)
+            .textTheme
+            .bodySmall
+            ?.copyWith(fontSize: 9),  // 删除了 decoration: TextDecoration.underline
+      )).marginOnly(top: 6);
 }
 
 // max 300 x 60
